0f47f8fad5da47f6ee790ec7b00fa15ff0313989
Willie Bjornbom <willie.bjornbom@ancon.io>
2019-09-02 07:34:26 +0200

    Updates

---
+++ precache-manifest.f554fdc2992590db32576ae40608a0c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852ed4fd9feab085512f73680d615b86",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce9d249c172e5cf4020b",
+    "url": "/monsters-rolodex/static/css/main.d73ce731.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "ce9d249c172e5cf4020b",
+    "url": "/monsters-rolodex/static/js/main.e4e2b861.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
